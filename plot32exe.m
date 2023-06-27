e1e9cf32135e9c4b3bae0fee5e2fdc9c00f7a2af
cuppy124 <137880279+cuppy124@users.noreply.github.com>
2023-06-27 19:27:23 +0800

    Update and rename plotcase32exe.m to plot32exe.m

--- plot32exe.m
+++ plot32exe.m
@@ -1,4 +1,4 @@
-%初始化lamda=2;M=1000;x=ones(M,1);timetotal=zeros(30,1);atl=1;yy=zeros(30,M);xx=nnxnum(M);
+%初始化lamda=2;M=10000;x=ones(M,1);timetotal=zeros(30,1);atl=1;yy=zeros(30,M);xx=nnxnum(M);
 tic
 newx=iteration(x,lamda);
 yy(atl,:)=newx;
